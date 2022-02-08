697b6e62c3f89ef80813da93e2e9f45b425ceeff
Kyle <Kyle.Plummer@revature.com>
2022-02-08 15:42:30 -0600

    Authorization model for transmitting user auth info, Completed stubbing out the controller methods for ActionItemController.

--- src/main/java/com/revature/revassistant/beans/controllers/ActionItemController.java
+++ src/main/java/com/revature/revassistant/beans/controllers/ActionItemController.java
@@ -1,8 +1,12 @@
 package com.revature.revassistant.beans.controllers;
 
 import com.revature.revassistant.models.ActionItem;
+import com.revature.revassistant.models.Authorization;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+import java.util.List;
 
 @RestController //@ResponseBody and @Controller are implied by @RestController
 @RequestMapping("/actionItems")
@@ -22,12 +26,26 @@ public class ActionItemController {
     }
 
     //post new action item
-    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     public void newActionItem(@RequestBody ActionItem item) {
 
     }
 
+    //update one specific action item
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public void updateActionItem(@RequestBody ActionItem item, @PathVariable Integer id) {
+
+    }
 
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteActionItem(@PathVariable Integer id) {
 
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<ActionItem> getAllActionItemsForUser(@RequestBody Authorization auth) {
+
+        return null;
+    }
 }
