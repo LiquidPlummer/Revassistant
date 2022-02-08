272661e4e5cf933ce4519126084d4a6b36705324
Kyle <Kyle.Plummer@revature.com>
2022-02-08 14:54:45 -0600

    Initial commit of files from spring boot initializer including start web and jpa dependencies.

---
+++ src/main/java/com/revature/revassistant/RevassistantApplication.java
@@ -0,0 +1,13 @@
+package com.revature.revassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevassistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevassistantApplication.class, args);
+	}
+
+}
